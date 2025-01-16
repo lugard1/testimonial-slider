f64ecb0829aca027a849a00e107af2743bf8e6f4
Lugard Agu <107192340+lugard1@users.noreply.github.com>
2025-01-16 20:28:15 +0100

    Set timeout and reset for the testimonials

--- index.js
+++ index.js
@@ -34,6 +34,18 @@ const usernameEl = document.querySelector('.username')
 
 let idx = 0;
 
+updateTestimonial();
+
 function updateTestimonial () {
-  const {name, photoUrl, text} = testimonials[idx]
+  const {name, photoUrl, text} = testimonials[idx];
+  imgEl.src = photoUrl;
+  textEl.innerText = text;
+  usernameEl.innerText = name;
+  idx++;
+  if(idx === testimonials.length){
+    idx = 0;
+  }
+  setTimeout(()=> {
+    updateTestimonial();
+  }, 15000)
 }
